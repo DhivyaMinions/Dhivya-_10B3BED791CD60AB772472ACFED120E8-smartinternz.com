9e8b23b88f81cc818aa849026f6b2ee3ef6c0c5d
DhivyaMinions <144201342+DhivyaMinions@users.noreply.github.com>
2023-09-06 12:28:08 +0530

    Add files via upload

---
+++ factorial.py
@@ -0,0 +1,6 @@
+#Write program that determine wheather a year enter by the User is a leap year year are not using if else statement
+number=int(input())
+if number%4==0:
+  print("leap year")
+else:
+  ptint("not leap year")
